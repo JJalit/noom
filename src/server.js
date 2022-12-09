d7159dc3ad676c14af90cd5882185445f22fdddb
jaeha <jaeha23@naver.com>
2022-12-09 15:26:22 +0900

    #1 CHAT WITH WEBSOCKETS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import WebSocket from 'ws';
+import http from 'http';
 
 const app = express();
 
@@ -9,4 +11,26 @@ app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on('connection', socket => {
+  sockets.push(socket);
+  socket['nickname'] = 'Anon';
+  console.log('Connected to Browser ✅');
+  socket.on('close', () => console.log('Disconnected from the Browser ❌'));
+  socket.on('message', msg => {
+    const message = JSON.parse(msg.toString('utf8'));
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+      case 'nickname':
+        socket['nickname'] = message.payload;
+    }
+  });
+  socket.send('Hello!');
+});
+
+server.listen(3000, handleListen);
